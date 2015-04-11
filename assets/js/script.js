523816091a382e207384c9019c891a73ba98250d
Ire Aderinokun <contact@ireade.co.uk>
2015-04-11 19:29:32 +0100

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+	$('.timeline-item .icon').hover(function() {
+		$(this).siblings('.more-info').toggleClass('more-info-show animated pulse');
+		$(this).siblings('.more-info').toggleClass('activated');
+	});
+
+	$('.more-info').hover(function() {
+		$(this).toggleClass('more-info-show');
+		$(this).siblings('.more-info').toggleClass('activated');
+	});
+
+	$('.cancel').on('click', function() {
+		$('.more-info').removeClass('more-info-show animated pulse activated');
+	});
+
+});
